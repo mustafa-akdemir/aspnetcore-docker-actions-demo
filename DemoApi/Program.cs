a2860f9e863994e5baef548ffb6c131b0d31e309
mustafa-akdemir <mustafa8011@outlook.com>
2025-08-24 21:55:58 +0300

    Demo: change greeting to v2

--- DemoApi/Program.cs
+++ DemoApi/Program.cs
@@ -4,7 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => Results.Ok(new {
-    message = "Hello from GitHub Actions + WSL + Docker + Actions ✅",
+    message = "Hello from GitHub Actions + WSL + Docker + Actions v2 ✨",
     version = Environment.GetEnvironmentVariable("VERSION") ?? "dev",
     time = DateTimeOffset.UtcNow
 }));
